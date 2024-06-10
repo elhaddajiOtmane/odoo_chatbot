87ba3509e160e2a7364a98baf6e67b675acfc02e
ELHADADJI Otmane <devconnect7@gmail.com>
2024-06-10 08:20:29 +0100

    feat : create endpoint for create lead from chatbot

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import xmlrpc.client
+
+app = Flask(__name__)
+
+url = 'https://webnexa.odoo.com'
+db = 'webnexa'
+username = 'oelhaddaji@webnexa.net'
+password = 'N53C#x?pdm93<'
+
+@app.route('/create_lead', methods=['POST'])
+def create_lead():
+    data = request.json
+    name = data.get('name')
+    contact_name = data.get('contact_name')
+    email_from = data.get('email_from')
+    phone = data.get('phone')
+
+    # Authenticate
+    common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+    uid = common.authenticate(db, username, password, {})
+    if uid:
+        models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+        lead_id = models.execute_kw(db, uid, password, 'crm.lead', 'create', [{
+            'name': name,
+            'contact_name': contact_name,
+            'email_from': email_from,
+            'phone': phone,
+        }])
+        return jsonify({'status': 'success', 'lead_id': lead_id})
+    else:
+        return jsonify({'status': 'failed', 'error': 'Authentication failed'}), 401
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
